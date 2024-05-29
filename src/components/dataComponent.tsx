808a8129e4c81ddbd576a23901bda402edc2e4a3
Maduranga Senadheera <lilans@voizzit.com>
2024-05-29 12:19:22 +0400

    added basic layout and dashboard

--- src/components/dataComponent.tsx
+++ src/components/dataComponent.tsx
@@ -3,7 +3,7 @@ const fetchApiData = () => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve('Data loaded!');
-    }, 2000); // Simulating a 2-second delay for data fetching
+    }, 5000); // Simulating a 2-second delay for data fetching
   });
 };
 
@@ -11,5 +11,5 @@ const fetchApiData = () => {
 export default async function  () {
   const apidata = await fetchApiData(); // This can be any async function, like an API call
   console.log('apidata',apidata); 
-  return <div>{apidata}</div>;
+  return <>{apidata  && <div>{apidata}</div>}</>;
 };
